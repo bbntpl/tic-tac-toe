bbe65f1a9a4636cf3be5c177ce3b4a8d329346ae
bvrbryn445 <law911012eqw@gmail.com>
2022-01-21 20:57:21 -0500

    feat: ai move implementation

--- index.js
+++ index.js
@@ -2,36 +2,55 @@ const displayController = (() => {
     const tilesEl = document.querySelectorAll('.ttt__tiles');
     const overlayEl = document.querySelector('.ttt__outcome__overlay');
     const _clearTiles = () => {
-        tilesEl.forEach(el => el.firstChild && el.removeChild(el.firstChild));
+        tilesEl.forEach(el => {
+            while (el.firstChild) {
+                el.removeChild(el.firstChild);
+            }
+        });
     }
     const _toggleClass = (rmvClass, addClass) => {
         overlayEl.classList.remove(rmvClass);
         overlayEl.classList.add(addClass);
     };
-    const insertPiece = (el, tttTile, player) => {
-        if (el.textContent !== '' && tttTile !== '') return;
+    const _insertChildNode = (el, text) => {
         const parent = document.createElement('div');
         parent.classList.add('xo');
         el.append(parent);
-        parent.textContent = player.getPiece();
+        parent.textContent = text;
+    }
+    const insertPiece = (el, tttTile, player) => {
+        if (el.textContent !== '' && tttTile !== '') return;
+        _insertChildNode(el, player.getPiece());
     }
     const changeText = (el, text) => el.textContent = text;
-    const toggleOverlay = (endGame) => endGame ? _toggleClass('hide', 'show') : _toggleClass('show', 'hide');
+    const toggleOverlay = (endGame) =>
+        endGame ? _toggleClass('hide', 'show')
+            : _toggleClass('show', 'hide');
     const redisplayGameboard = () => {
         _clearTiles();
         _toggleClass('show', 'hide');
     }
+    const updateBoard = (board) => {
+        _clearTiles();
+        tilesEl.forEach((el, n) => {
+            const minusOne = n % 3 === 0 ? 0 : 1;
+            const xy = [Math.ceil(n / 3) - minusOne, n % 3];
+            _insertChildNode(el, board[xy[0]][xy[1]]);
+        })
 
+    }
     return {
         redisplayGameboard,
         toggleOverlay,
         insertPiece,
         changeText,
-        overlayEl
+        updateBoard,
+        overlayEl,
     }
 })();
 
 //TTT is the acronym for Tic-Tac-Toe
+//Gameboard Module Pattern
 const TTT = (() => {
     let currentTurn = 'x';
     let gamemode = localStorage.getItem('gamemode') ? localStorage.getItem('gamemode') : 'ai';
@@ -41,10 +60,8 @@ const TTT = (() => {
         ['', '', '']];
     const possibleOutcomes = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
     const _tileNumToIndices = (n) => {
-        // 0 1 2
-        // 3 4 5
-        // 6 7 8
         if (!n) return [0, 0];
+        //Prevent using Math.ceil to whole number (n)
         const minusOne = n % 3 === 0 ? 0 : 1;
         return [Math.ceil(n / 3) - minusOne, n % 3];
     }
@@ -57,7 +74,6 @@ const TTT = (() => {
     };
     const updateBoard = (piece, boardPos) => {
         if (Array.isArray(boardPos)) {
-            if (board[boardPos[0]][boardPos[1]] === '') return;
             board[boardPos[0]][boardPos[1]] = piece;
         } else {
             const xy = _tileNumToIndices(boardPos);
@@ -70,17 +86,32 @@ const TTT = (() => {
                 board[i][j] = '';
             }
         }
-        currentTurn = 'x';
+        setTurn('x');
     };
-    const filterFilledBoard = (outcome, piece) => {
-        const winningLen = outcome.filter(index => getBoardPos(index) === piece).length;
+    const filterFilledBoard = (outcome, piece, board = null) => {
+        //if there's another board passed as an argument
+        //use that board instead
+        const requestedBoard = (i) => {
+            const xy = _tileNumToIndices(i);
+            return board !== null ? board[xy[0]][xy[1]] : getBoardPos(i);
+        }
+        const winningLen = outcome.filter(i => requestedBoard(i) == piece).length;
         return winningLen;
     }
     const setGamemode = (newGamemode) => gamemode = newGamemode;
     const getGamemode = () => gamemode;
-    const isThereAWinner = (piece) => {
-        const checkForAWinner = possibleOutcomes.find(outcome => filterFilledBoard(outcome, piece) === 3);
-        return !!checkForAWinner;
+    const isThereAWinner = (piece, board = null) => {
+        const checkForAWinner = possibleOutcomes.find(outcome => filterFilledBoard(outcome, piece, board) === 3);
+        return checkForAWinner ? piece : null;
+    }
+    const isBoardFull = (customBoard = null) => {
+        let openSpots = 0;
+        if (customBoard) {
+            customBoard.forEach(c => c.forEach(v => v == '' ? openSpots++ : openSpots));
+        } else {
+            board.forEach(c => c.forEach(v => v == '' ? openSpots++ : openSpots));
+        }
+        return openSpots === 0;
     }
     return {
         updateBoard,
@@ -92,6 +123,7 @@ const TTT = (() => {
         setGamemode,
         getGamemode,
         isThereAWinner,
+        isBoardFull
     }
 })();
 
@@ -105,7 +137,7 @@ const Player = (name, xo) => {
 
 const AI = (name, xo) => {
     const prototypePlayer = Player(name, xo);
-    const move = (p1, p2, currentPlayer) => setTimeout(() => bestMove(p1, p2, currentPlayer), 200);
+    const move = (p1, p2, currentPlayer) => bestMove(p1, p2, currentPlayer);
     return Object.assign({}, prototypePlayer, { move })
 }
 
@@ -120,11 +152,9 @@ const game = (() => {
         const p2 = TTT.getGamemode() === 'ai' ? AI('AI', 'o') : Player('Player2', 'o');
         return { p1, p2 }
     }
-    function _AIMove(p1, p2) {
-        const currentPlayer = TTT.getTurn() === 'x' ? p1 : p2;
-        const nextPlayer = TTT.getTurn() === 'x' ? p2 : p1;
+    function _AIMove(p1, p2, currentPlayer) {
         p2.move(p1, p2, currentPlayer);
-        _changeTurn(currentPlayer, nextPlayer);
+        _changeTurn(p2, p1);
     }
 
     function _playerMove(p1, p2) {
@@ -132,12 +162,19 @@ const game = (() => {
         tilesEl.forEach((el, i) => {
             //Each move
             el.onclick = (e) => {
-                const currentPlayer = TTT.getTurn() === 'x' ? p1 : p2;
-                const nextPlayer = TTT.getTurn() === 'x' ? p2 : p1;
-                if (!TTT.getBoardPos(i)) {
+                const isAiCurrentPlayer = TTT.getGamemode() === 'ai' && TTT.getTurn() === 'o';
+                if (!TTT.getBoardPos(i) && !isAiCurrentPlayer) {
+                    const currentPlayer = TTT.getTurn() === 'x' ? p1 : p2;
+                    const nextPlayer = TTT.getTurn() === 'x' ? p2 : p1;
                     displayController.insertPiece(el, TTT.getBoardPos(i), currentPlayer);
                     TTT.updateBoard(TTT.getTurn(), i);
                     _changeTurn(currentPlayer, nextPlayer);
+                    const isAiTurn = TTT.getGamemode() === 'ai' && !TTT.isThereAWinner(currentPlayer.getPiece());
+                    if (isAiTurn) {
+                        setTimeout(() => {
+                            _AIMove(p1, p2, currentPlayer);
+                        }, 700)
+                    }
                 }
             };
         })
@@ -149,17 +186,10 @@ const game = (() => {
         startGame();
     }
 
+    //  add event listener to overlay which
+    //  redisplay the board as if the game refreshed
     function _clickOverlayToHide() {
-        displayController.overlayEl.addEventListener('click', () => {
-            _restartGame();
-        })
-    }
-
-    //change the turn for the other player
-    function _changeTurn(currentPlayer, nextPlayer) {
-        _checkIfThereIsAWinner(currentPlayer);
-        displayController.changeText(userTurnEl, `${nextPlayer.getName()}\'s turn`);
-        TTT.getTurn() === 'x' ? TTT.setTurn('o') : TTT.setTurn('x');
+        displayController.overlayEl.addEventListener('click', _restartGame.bind());
     }
 
     //display winner when it is detected that there's a winner
@@ -169,8 +199,34 @@ const game = (() => {
     }
 
     function _checkIfThereIsAWinner(currentPlayer) {
-        const isGameover = TTT.isThereAWinner(TTT.getTurn());
-        _endGame(isGameover, currentPlayer);
+        const isGameover = TTT.isThereAWinner(currentPlayer.getPiece());
+        //It ends with a draw if the board is filled without a winner
+        if (TTT.isBoardFull() && !isGameover) {
+            displayController.toggleOverlay(TTT.isBoardFull());
+            displayController.changeText(winnerTxt, ' draw');
+        } else {
+            _endGame(isGameover, currentPlayer);
+        }
+    }
+
+    //change the turn for the other player
+    function _changeTurn(currentPlayer, nextPlayer) {
+        _checkIfThereIsAWinner(currentPlayer);
+        displayController.changeText(userTurnEl, `${nextPlayer.getName()}\'s turn`);
+        TTT.getTurn() === 'x' ? TTT.setTurn('o') : TTT.setTurn('x');
+    }
+
+    function equals3(a, b, c) {
+        return a == b && b == c && a != '';
+    }
+
+    function checkBestMoveResult(board) {
+        let winner = TTT.isThereAWinner('o', board);
+        if (winner == null && TTT.isBoardFull(board)) {
+            return 'tie';
+        } else {
+            return winner;
+        }
     }
 
     //switch the opponent between a player and AI
@@ -190,20 +246,28 @@ const game = (() => {
         const fetchedGamemode = localStorage.getItem('gamemode');
         const currentPlayerInputValue = fetchedGamemode === 'player' ? true : false;
         playerInput.checked = currentPlayerInputValue;
-        _clickOverlayToHide(); //add event listener to overlay
+        _clickOverlayToHide();
     }
 
     const startGame = () => {
         const p1 = _players().p1;
         const p2 = _players().p2;
         displayController.changeText(userTurnEl, `${p1.getName()}\'s turn`);
-        if (TTT.getGamemode() === 'ai' && TTT.getTurn('o')) {
-            _AIMove(p1, p2);
-        } else {
-            _playerMove(p1, p2);
-        }
+        _playerMove(p1, p2);
     }
-    return { toggleGamemode, initializePreparationBeforeTTT, startGame }
+
+    const updateBoard = (piece, boardPos) => {
+        TTT.updateBoard(piece, boardPos);
+        const updatedBoard = TTT.getBoard();
+        displayController.updateBoard(updatedBoard);
+    }
+    return {
+        toggleGamemode,
+        initializePreparationBeforeTTT,
+        startGame,
+        updateBoard,
+        checkBestMoveResult
+    }
 })()
 
 game.toggleGamemode();
--- minimax.js
+++ minimax.js
@@ -1,66 +1,60 @@
-function bestMove() {
-    // AI to make its turn
+function bestMove(P1, P2) {
+  // AI to make its turn
+  let bestScore = -Infinity;
+  let move;
+  const ai = P2.getPiece();
+  const board = TTT.getBoard();
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      // Is the spot available?
+      if (board[i][j] == '') {
+        board[i][j] = ai;
+        let score = minimax(board, 0, false);
+        board[i][j] = '';
+        if (score > bestScore) {
+          bestScore = score;
+          move = { i, j };
+        }
+      }
+    }
+  }
+  game.updateBoard(ai, [move.i, move.j]);
+}
+
+let scores = { x: 1, o: -1, tie: 0 };
+
+function minimax(board, depth, isMaximizing) {
+  let result = game.checkBestMoveResult(board);
+  if (result !== null) {
+    return scores[result];
+  }
+  if (isMaximizing) {
     let bestScore = -Infinity;
-    let move;
-    const board = TTT.getBoardPos();
-    console.log(TTT.getBoardPos());
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         // Is the spot available?
         if (board[i][j] == '') {
-          board[i][j] = ai;
-          let score = minimax(board, 0, false);
+          board[i][j] = 'o';
+          let score = minimax(board, depth + 1, false);
           board[i][j] = '';
-          if (score > bestScore) {
-            bestScore = score;
-            move = { i, j };
-          }
+          bestScore = Math.max(score, bestScore);
         }
       }
     }
-    board[move.i][move.j] = ai;
-    currentPlayer = human;
+    return bestScore;
+  } else {
+    let bestScore = Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // Is the spot available?
+        if (board[i][j] == '') {
+          board[i][j] = 'x';
+          let score = minimax(board, depth + 1, true);
+          board[i][j] = '';
+          bestScore = Math.min(score, bestScore);
+        }
+      }
+    }
+    return bestScore;
   }
-  
-  let scores = {
-    x: 10,
-    o: -10,
-    tie: 0
-  };
-  
-  function minimax(board, depth, isMaximizing) {
-    let result = checkWinner();
-    if (result !== null) {
-      return scores[result];
-    }
-  
-    if (isMaximizing) {
-      let bestScore = -Infinity;
-      for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-          // Is the spot available?
-          if (board[i][j] == '') {
-            board[i][j] = ai;
-            let score = minimax(board, depth + 1, false);
-            board[i][j] = '';
-            bestScore = max(score, bestScore);
-          }
-        }
-      }
-      return bestScore;
-    } else {
-      let bestScore = Infinity;
-      for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-          // Is the spot available?
-          if (board[i][j] == '') {
-            board[i][j] = human;
-            let score = minimax(board, depth + 1, true);
-            board[i][j] = '';
-            bestScore = min(score, bestScore);
-          }
-        }
-      }
-      return bestScore;
-    }
-  }
+}
